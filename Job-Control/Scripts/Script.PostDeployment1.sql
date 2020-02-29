d4201a8c1ded39d4f0e83cccdb7393339c417a09
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-02-29 15:09:46 -0500

    Add JOB_MONITOR_EVENT table and post deployment script to populate it.

---
+++ Job-Control/Scripts/Script.PostDeployment1.sql
@@ -0,0 +1,39 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF NOT EXISTS (
+	SELECT 1 
+	FROM [dbo].[JOB_MONITOR_EVENT]
+	WHERE [JOB_MONITOR_EVENT_DESCRIPTION] = 'JOB COMPLETED SUCCESSFULLY'
+)
+INSERT [dbo].[JOB_MONITOR_EVENT] (
+	[JOB_MONITOR_EVENT_DESCRIPTION]
+)
+VALUES (
+	'JOB COMPLETED SUCCESSFULLY'
+);
+GO
+
+
+IF NOT EXISTS (
+	SELECT 1 
+	FROM [dbo].[JOB_MONITOR_EVENT]
+	WHERE [JOB_MONITOR_EVENT_DESCRIPTION] = 'JOB STEP COMPLETED SUCCESSFULLY'
+)
+INSERT [dbo].[JOB_MONITOR_EVENT] (
+	[JOB_MONITOR_EVENT_DESCRIPTION]
+)
+VALUES (
+	'JOB STEP COMPLETED SUCCESSFULLY'
+);
+GO
+
