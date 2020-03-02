f4143cef37cf909055de94b1134d54c8751eb375
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-03-02 05:54:01 -0500

    Add CRM_SQL_AGENT_JOB_QUERIES.sql.

---
+++ Job-Control/Scripts/CRM_SQL_AGENT_JOB_QUERIES.sql
@@ -0,0 +1,31 @@
+﻿
+--
+-- queries to retrieve job_id and step_uid for our jobs
+--
+;WITH CTE_JOB_NAME AS (
+	SELECT 
+		JOB_NAME
+	FROM 
+	(
+		VALUES
+			(N'CRM DATA MART DAILY ETL')
+		,	(N'CRM SSAS CUBE PROCESS')
+		,	(N'CRM DASHBOARD DAILY REFRESH')
+	)
+	AS JOBS(JOB_NAME)
+)
+
+SELECT --*
+	j.[name]
+,	s.job_id
+,	s.step_id
+,	s.step_name
+,	s.step_uid
+FROM msdb.dbo.sysjobsteps s
+JOIN msdb.dbo.sysjobs j
+	ON j.job_id = s.job_id
+JOIN CTE_JOB_NAME n
+	ON n.JOB_NAME = j.[name]
+ORDER BY j.[name], s.step_id;
+
+
